1c2dd3febf3be8a68790fde54d16d1f9bb51d8fa
Javier Gutierrez <dukintosh@gmail.com>
2022-09-06 07:55:32 -0500

    ya medio existe create Video game y show Video gam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ const GenresArray = require('./GenresArray.js')
 const PlatformsArray = require('./platformsArray.js')
 function App () {
   return (
+<>
     <div className="App nes-container with-title">
-
+      <p className="title">container:form</p>
       <p className="nes-container is-dark with-title">
         <p className="title">Henry Videogames</p>
         <p>This is some kind of back-end app.</p>
@@ -68,9 +69,127 @@ function App () {
           </div>
           <button type="button" className="nes-btn is-inline">+</button>
         </div>
-
         </p>
     </div>
+
+<div className="App nes-container with-title">
+  <p className="title">container:home</p>
+    <p className="nes-container is-dark with-title">
+      <p className="title">::home_header::</p>
+        <p>[home_greet]</p>
+        <div className="lists">
+          <ul className="nes-list is-disc">
+            <li>
+              Filtrar...:
+            </li>
+          </ul>
+        </div>
+        <div className="lists">
+          <ul className="nes-list is-circle">
+            <li>Por género: [select]&nbsp;&nbsp;&nbsp;   Por autor:  [select]&nbsp;&nbsp;&nbsp;    Por Rating
+            &nbsp;<i className="nes-icon is-small star is-half"></i>
+          <i className="nes-icon is-small is-transparent star"></i>
+          <i className="nes-icon is-small is-transparent star"></i>
+          <i className="nes-icon is-small is-transparent star"></i>
+          <i className="nes-icon is-small is-transparent star"></i>
+            </li>
+          </ul>
+        </div>
+      </p>
+      <section className="basic-grid">
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::1</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::2</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::3</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::4</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::5</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::6</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::7</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::8</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::9</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::10</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::11</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::12</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::13</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::14</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>      <div className="nes-container is-dark with-title is-primary">
+        <p className="title">VideoGame::15</p>
+        <p>Hola! soy una imagen dizque :D</p>
+          <p>Y yo soy los generos</p>
+      </div>
+    </section>
+    </div>
+    <div className="lists">
+  <ul className="nes-list is-disc">
+    <li>Good morning.</li>
+    <li>Thou hast had a good night&apos;s sleep, I hope.</li>
+    <li>Thou hast had a good afternoon</li>
+    <li>Good night.</li>
+  </ul>
+</div>
+
+<div className="lists">
+  <ul className="nes-list is-circle">
+    <li>Good morning.</li>
+    <li>Thou hast had a good night&apos;s sleep, I hope.</li>
+    <li>Thou hast had a good afternoon</li>
+    <li>Good night.</li>
+  </ul>
+</div>
+
+  </>
   )
 }
 
