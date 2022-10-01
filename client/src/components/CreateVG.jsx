520c4c475ff9ea0c15b46b746e614f2269b4fd3f
Javier Gutierrez <dukintosh@gmail.com>
2022-09-30 22:12:31 -0500

    falta menos

--- client/src/components/CreateVG.jsx
+++ client/src/components/CreateVG.jsx
@@ -1,38 +1,40 @@
 // import React from 'react'
-// import GenresArray from './GenresArray'
-// import PlatformsArray from './platformsArray'
+import GenresArray from './GenresArray'
+import PlatformsArray from './platformsArray'
 // import DropdownFilters from './DropdownFilters'
-// import Rating from './Rating'
-// import Input from './Input'
-// import Textarea from './Textarea'
+import Rating from './Rating'
+import Input from './Input'
+import Textarea from './Textarea'
 import WhiteContainer from './WhiteContainer'
 
 const createVG = () => {
-  return (<>
+  let handleInputChange = (e) => {
+    setInput( prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  }
+return (<>
     <h1>Create Videogame:</h1>
     <WhiteContainer title="Crear VideoJuego:" >
       <p>Estas son las instrucciones para crear un Videogame. Presiona las teclas para hacer algo y así. Despues pícale a otro botón y mueve el mouse y listo.</p>
       <p>Here be dragons:</p>
     </WhiteContainer>
-  </>)
-}
 
-export default createVG
-/*
     <WhiteContainer >
-      <Input label="Nombre: " name="name" />
+      <Input onChange={handleInputChangeimage.png} label="Nombre: " name="name" />
       <Textarea />
       <Input label="Fecha de lanzamiento: " name="release" inline="true" />
       <Rating size="medium"/>
-      <Dropdown label="Género: ">{
+      <select label="Género: ">{
         GenresArray.map((unGenero, key) => {
           return (
             <option className="is-inline" key={key} value={key}>
               {unGenero}
             </option>)
         })
-      }</Dropdown>
-      <Dropdown label="Plataforma: ">{
+      }</select>
+      <select label="Plataforma: ">{
         PlatformsArray.map((unaPlataforma, key) => {
           return (
             <option className="is-inline" key={key} value={key}>
@@ -40,6 +42,9 @@ export default createVG
             </option>
           )
         })
-      }</Dropdown>
+      }</select>
     </WhiteContainer>
-*/
+  </>)
+}
+
+export default createVG
