46253aface82224f3af5aafd13a8949d064a937f
Javier Gutiérrez <dukintosh@gmail.com>
2023-02-15 05:40:37 -0600

    idonnou

---
+++ api/src/seed/videogamesSeed.js
@@ -0,0 +1,25 @@
+const gamesrequest = require  ('./games.page1.json');
+const gamesrequest2 = require ('./games.page2.json');
+const gamesrequest3 = require ('./games.page3.json');
+const gamesrequest4 = require ('./games.page4.json');
+const gamesrequest5 = require ('./games.page5.json');
+
+const parsePage = (page) => {
+  return page.results.map((el) => { 
+    const buffer = {
+      id: el.id,
+      img: el.background_image,
+      name: el.name, 
+      description: "%%description placeholder%%",
+      released: el.released, 
+      rating: el.rating,
+      platforms: el.parent_platforms.map(lel => lel.platform.name),
+      genres: el.genres.map(lol => lol.name),
+    }
+    return buffer;
+  })
+};
+
+let archivos = [gamesrequest,gamesrequest2,gamesrequest3,gamesrequest4,gamesrequest5];
+
+module.exports = archivos.map(el => parsePage(el)).flat();
